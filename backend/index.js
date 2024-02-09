e0616034b9138476964757059572aad52ce89707
rohitkdv <159414892+rohitkdv@users.noreply.github.com>
2024-02-09 16:52:27 +0530

    MongoDb Connection (HKS)

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
@@ -7,7 +8,27 @@ app.get('/news', (req, res)=>{
   res.send(JSON.stringify(newslist));
 })
 
+
+
+
 const PORT = 8080;
+
+
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+	try{
+		const conn = await mongoose.connect(process.env.MONGODB_URL);
+		console.log('Database connected')
+	} catch (error) {
+		console.log(`error in mongodb ${error}`)
+	}
+};
+
+connectDB();
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

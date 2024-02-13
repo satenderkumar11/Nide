007dcc179b165715bb86e45296c75a62ccae5bc3
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-02-13 22:48:57 +0530

    Updated product api and its schema.

--- backend/model/Product.js
+++ backend/model/Product.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const productSchema = new Schema({
-    prodId: {type: Number, required: true, unique: true},
-    prodName: {type: String, required: true, unique: true},
-    prodImg: [{
-        data: Buffer,
-        contentType: String,
-    }],
-    prodSpec: {type: String},
+    prodId: {type: Number, required : true, unique: true},
+    prodTitle: {type: String, required: true, unique: true},
+    prodModelNumber: {type: String, required: true, unique: true},
+    prodeVideoUrl: {type: String},
+    prodImg: {type: String},
+    prodSpec: {type: String}, 
     prodCtgry: {type: String, requrired: true}
 });
 

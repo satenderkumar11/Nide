83ad5e96f4075e267db4a93c6a8430774a743d23
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-02-14 22:56:39 +0530

    updated query api: added more functionality

--- backend/controller/Queries.js
+++ backend/controller/Queries.js
@@ -19,3 +19,32 @@ exports.fetchAllQuery = async (req, res) => {
         res.status(400).json(err);
     }
 }
+
+exports.updateQueryById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const update = req.body;
+        const updatedQuery = await QueryModel.findByIdAndUpdate(id, update, {new: true});
+
+        if(!updatedQuery){
+            return res.status(404).json({message: "Query Doesn't exist."});
+        }
+
+        res.status(201).json(updatedQuery);
+    } catch (err) {
+        res.status(500).json({messge: err.message})
+    }
+}
+
+exports.deleteQuery = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const query = await QueryModel.findByIdAndDelete(id);
+        if(!query) {
+            return res.status(404).send({error: "product not found"});
+        }
+        res.status(200).json(query);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
--- backend/model/Product.js
+++ backend/model/Product.js
@@ -2,29 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-<<<<<<< Updated upstream
-    
-    
     title: {type: String, required: true},
     model: {type: String, required: true, unique: true},
     videoUrl: {type: String},
     img: {type: String},
     specification: {type: String, required: true}, 
     category: {type: String, required: true},
-    
-
 }, {timestamps: true});
   
 exports.ProductModel = mongoose.model('Product', productSchema);
-=======
-  prodId: { type: Number, required: true, unique: true },
-  prodTitle: { type: String, required: true, unique: true },
-  prodModelNumber: { type: String, required: true, unique: true },
-  prodVideoUrl: { type: String },
-  prodImg: { type: String },
-  prodSpec: { type: String, required: true },
-  prodCategory: { type: String, required: true },
-});
-
-exports.ProductModel = mongoose.model("Product", productSchema);
->>>>>>> Stashed changes

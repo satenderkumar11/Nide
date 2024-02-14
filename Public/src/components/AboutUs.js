3b7e959ff28b3bfc256825c65c539855b1bc79d9
satenderkumar11 <77235364+satenderkumar11@users.noreply.github.com>
2024-02-14 18:48:52 +0530

    added values section

--- Public/src/components/AboutUs.js
+++ Public/src/components/AboutUs.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import LandingVideo from './LandingVideo';
+import { TbTargetArrow } from "react-icons/tb";
+import { FaEye, FaRegLightbulb } from "react-icons/fa";
 
 const AboutUs = () => {
   return (
@@ -25,6 +27,30 @@ const AboutUs = () => {
           electric motors, Brushless motors, Traction electric motors, Starter
           motors, Alternators and Induction electric motors respectively.
         </p>
+      </div>
+
+      {/* Workplace culture */}
+      <div className='w-3/5 mx-auto my-4 px-6'>
+        <h4 className='font-bold text-xl my-4 text-center uppercase'>Core values</h4>
+        <div className='grid grid-cols-3 text-neutral-50 gap-3'>
+          <div className='bg-gradient-to-b from-blue-950 to-blue-800 rounded-lg flex flex-col items-center p-4'>
+            <span className='text-4xl'><TbTargetArrow /></span>
+            <h6 className='font-semibold'>MISSION</h6>
+            <p className='text-sm text-neutral-200 mt-2'>Transmit value, Realize dream, Take responsibility</p>
+          </div>
+          <div className='bg-gradient-to-b from-blue-950 to-blue-800 rounded-lg flex flex-col items-center p-4'>
+            <span className='text-4xl'><FaEye /></span>
+            <h6 className='font-semibold'>VISION</h6>
+            <p className='text-sm text-neutral-200 mt-2'>Provide customer with optimized solution to motor manufacturing</p>
+          </div>
+          <div className='bg-gradient-to-b from-blue-950 to-blue-800 rounded-lg flex flex-col items-center p-4'>
+            <span className='text-4xl'><FaRegLightbulb /></span>
+            <h6 className='font-semibold'>PHILOSOPHY</h6>
+            <p className='text-sm text-neutral-200 mt-2'>
+              Serving, Professional, Prompt, Innovative
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );

5a4827f342a5c0f1499c3bf02160754f4d3ce354
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-02-15 10:15:33 +0530

    Updated Queries Schema

--- backend/model/Queries.js
+++ backend/model/Queries.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 
 const querySchema = new Schema({
     description: {type: String, required: true},
-    product: { type: Schema.Types.ObjectId, ref: 'ProductModel', required: true},
+    product: { type: Schema.Types.ObjectId, ref: 'ProductModel'},
     queryResolvedDate: {type: Date},
     user: {type: Schema.Types.ObjectId, ref: 'UserModel', required: true},
 }, {timestamps: true});

85a8553bd40b1b26d8816c58f666c821a2e0d510
rohitkdv <159414892+rohitkdv@users.noreply.github.com>
2024-02-15 11:47:07 +0530

    Update Job.js

--- backend/controller/Job.js
+++ backend/controller/Job.js
@@ -1,39 +1,52 @@
-const {JobModel} = require('../model/Job');
+const { JobModel } = require("../model/Job");
 
-exports.createJob = async(req, res) => {
+exports.createJob = async (req, res) => {
+  const JobItem = new JobModel(req.body);
 
-    const JobItem  = new JobModel(req.body);
-
-    try {
-        const doc = await JobItem.save();
-        res.status(201).json(doc);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-
-}
+  try {
+    const doc = await JobItem.save();
+    res.status(201).json(doc);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
 exports.fetchAllJobs = async (req, res) => {
-
-        try {
-            const docs = await JobModel.find();
-            console.log(docs);
-            res.status(200).json(docs);
-        } catch (err) {
-            res.status(400).json(err);
-            console.log(err);
-        }
-
-}
+  try {
+    const docs = await JobModel.find();
+    console.log(docs);
+    res.status(200).json(docs);
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err);
+  }
+};
 
 exports.fetchJobById = async (req, res) => {
 
-    const { id } = req.params;
-  
-    try {
-      const docs = await JobModel.findOne({JobId : id});
-      res.status(200).json(docs);
-    } catch (err) {
-      res.status(400).json(err);
-    }
+  const { id } = req.params;
+    console.log(id);
+  try {
+    const docs = await JobModel.findOne({ jobId: id });
+    console.log(docs);
+    res.status(200).json(docs);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.updateJobbyId = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    const docs = await JobModel.findOneAndUpdate(
+      { jobId: id },
+      { $set: updates },
+      { new: true }
+    );
+    res.status(200).json(docs);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
